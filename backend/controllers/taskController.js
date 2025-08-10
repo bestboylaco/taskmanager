36f5defd422c583f2d2e56cac1d2225463de4a2a
N10172505 <nguyenvobaodung@gmail.com>
2025-08-11 03:26:48 +1000

    Add task controller CRUD

---
+++ backend/controllers/taskController.js
@@ -0,0 +1,48 @@
+const Task = require('../models/taskModel');
+
+// @desc Get all tasks
+const getTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find();
+        res.status(200).json(tasks);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+// @desc Create a new task
+const createTask = async (req, res) => {
+    try {
+        const task = await Task.create(req.body);
+        res.status(201).json(task);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+// @desc Update a task
+const updateTask = async (req, res) => {
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        res.status(200).json(task);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+// @desc Delete a task
+const deleteTask = async (req, res) => {
+    try {
+        await Task.findByIdAndDelete(req.params.id);
+        res.status(204).send();
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+module.exports = {
+    getTasks,
+    createTask,
+    updateTask,
+    deleteTask
+};
